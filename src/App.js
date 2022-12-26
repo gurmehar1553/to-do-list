2bb7757accf155e902f5df83db914fde7effbf9c
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2022-12-26 23:10:06 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './index.css';
+import ToDoList from "./ToDoList";
 
-function App() {
+const App = ()=>{
+
+  
+  const [text,setText] = useState("");
+  const [listItem,setListItem] = useState([]);
+  const updateValue = (event)=>{
+    setText(event.target.value);
+  }
+  const addValue = ()=>{
+    setListItem((prevItem) => {
+      if(text === ""){
+        return [...prevItem];
+      }
+      return [...prevItem,text];
+    });
+    setText("")
+  }
+
+  const deleteItem = (id) => {
+    setListItem((prevItem)=>{
+      return prevItem.filter((arrElem,index)=>{
+        return index!==id;
+      })
+    })
+  }
+  
+  const textBoxdata = text;
+// Store textbox current value
+localStorage.setItem("firstnameval", textBoxdata);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outer-div">
+      <div className="inner-div">
+        <h1 className="heading ">TO-DO LIST</h1>
+        <div className="main-div">
+          <div style={{textAlign:"center"}}>
+        <input type="text" placeholder="Add a task" value={text} onChange={updateValue}/>
+        <span><button onClick={addValue}><i className="fa-solid fa-circle-plus plus-icon"></i></button></span>
+        </div>
+        <div className="second-div">
+          <ul type="none">
+            {/* <li><span><i class="fa-solid fa-circle-minus minus-icon"></i></span>{listItem}</li> */}
+            {listItem.map((itemVal,index)=>{
+              return <ToDoList key={index} id={index} text={itemVal} onSelect={deleteItem}/>
+            })}
+
+          </ul>
+        </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
